4daa568a631c0c2004ad967c4ca21379ef023866
Asmi-mihe <asmibaidya1@gmail.com>
2026-01-13 10:28:19 +0545

    Remove authentication and UI modules; add Flask app structure

--- backend/test_auth.py
+++
@@ -1,6 +0,0 @@
-from backend.auth import signup, login
-
-print(signup("ashley", "1234"))
-print(login("ashley", "1234"))
-print(login("ashley", "wrongpass"))
-# Test cases for authentication module
